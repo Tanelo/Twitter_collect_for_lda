5af59968b7b4e858b61b46ade5b034c8082c4f0a
Tanelo <tpetelot@gmail.com>
2021-10-06 00:45:27 +0200

    New changes

--- get_tweets.py
+++ get_tweets.py
@@ -72,6 +72,92 @@ def get_tweets_per_topic(topic,year,month,day,number_of_tweets):
 
     return df
 
+<<<<<<< Updated upstream
+=======
+def get_tweets_from_people(usernames,number_of_tweets):
+    """
+    """
+    #Giving nb of rows to pre-allocate and be memory efficient
+    df = pd.DataFrame(index=np.arange(0, len(usernames) * number_of_tweets), columns=['id', 'date', 'text','retweet_count'])
+    auth = tw.OAuthHandler(api_key['consumer_key'], api_key['consumer_secret'])
+    auth.set_access_token(api_key['access_key'], api_key['access_secret'])
+    api = tw.API(auth,wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True)
+    #geo_code can be a parameter
+    for k, username in enumerate(usernames):
+        try: 
+            tweets = tw.Cursor(api.user_timeline, screen_name = username).items(number_of_tweets)
+    
+            for i, tweet in enumerate(tweets):
+                df.loc[k*number_of_tweets + i] = [tweet.user.name, tweet.created_at, tweet.text, tweet.retweet_count]
+        except:
+            print("\n....There has beenproblem with the name of one twittos in the list...")
+            pass
+
+
+            
+
+    return df
+
+def get_tweets_with_comments(usernames,number_of_tweets):
+    df = pd.DataFrame(index=np.arange(0, len(usernames) * number_of_tweets), columns=['id','name', 'date', 'text','comments','retweet_count'])
+    #comments is a list
+    auth = tw.OAuthHandler(api_key['consumer_key'], api_key['consumer_secret'])
+    auth.set_access_token(api_key['access_key'], api_key['access_secret'])
+    api = tw.API(auth,wait_on_rate_limit=True,
+    )
+    for k, username in enumerate(usernames):
+        try: 
+            tweets = tw.Cursor(api.user_timeline, screen_name = username).items(number_of_tweets)
+    
+            for i, tweet in enumerate(tweets):
+                name = str(tweet.user.name)
+                tweet_id = tweet.id
+                replies=[]
+                if tweet.is_quote_status:
+                    print("is quote")
+
+                    
+                    for comment in tw.Cursor(api.search_tweets,q=name, result_type='recent').items(1000):
+                        if hasattr(comment, 'in_reply_to_status_id'):
+                            
+                            if (comment.in_reply_to_status_id==tweet_id):
+                                print("on a trouvé")
+                                replies.append([str(comment.text), str(comment.user.name)])
+
+                    df.loc[k*number_of_tweets + i] = [tweet.id,tweet.user.name, tweet.created_at, tweet.text, replies, tweet.retweet_count]
+
+                else:
+                    df.loc[k*number_of_tweets + i] = [tweet.id,tweet.user.name, tweet.created_at, tweet.text, replies, tweet.retweet_count]
+
+        except:
+            print("\n....There has beenproblem with the name of one twittos in the list...")
+            pass
+
+    return df
+
+def test_query(username, number_of_tweets):
+    auth = tw.OAuthHandler(api_key['consumer_key'], api_key['consumer_secret'])
+    auth.set_access_token(api_key['access_key'], api_key['access_secret'])
+    api = tw.API(auth,wait_on_rate_limit=True,
+    )
+
+    # tweets = tw.Cursor(api.user_timeline, screen_name = username).items(number_of_tweets)
+    name = str("NYU Stern")
+    tweet_id = 1442487073731190786
+    replies=[]
+    
+                    
+    for comment in tw.Cursor(api.search_tweets,q=name, result_type='recent', ).items(1000):
+        
+        if hasattr(comment, 'in_reply_to_status_id'):
+            print("on a trouvé")
+            if (comment.in_reply_to_status_id==tweet_id):
+                replies.append([str(comment.text), str(comment.user.name)])
+    print(replies)
+
+
+>>>>>>> Stashed changes
 def get_tweets_dataframe(topics,year,month,day,number_of_tweets):
     """
     args : topics, year, month , day are Strings, number_of_tweets is an int
@@ -99,7 +185,11 @@ def get_tweets_dataframe(topics,year,month,day,number_of_tweets):
 if __name__ == '__main__':
     # #alternative method: loop through multiple users
 	# # users = ['user1','user2']
+<<<<<<< Updated upstream
     number_of_tweets = 2
+=======
+    number_of_tweets = 50
+>>>>>>> Stashed changes
     #who = 'joebiden'
     #get_tweets(who, number_of_tweets)
 
@@ -107,4 +197,21 @@ if __name__ == '__main__':
     #full_df = pd.DataFrame(index=np.arange(0, number_of_tweets * len(topics)), columns=['id', 'date', 'text'])
     full_df = get_tweets_dataframe(topics, "2021","06","25",number_of_tweets)
 
+<<<<<<< Updated upstream
     full_df.to_csv("full_df.csv", index=False)
+=======
+    
+    file = open("dict.json", "r")
+    data = json.loads(file.read())
+    topics = list(data.keys())
+    new_df = get_tweets_with_comments(data[topics[-2]],10)
+    new_df.to_csv("good_df"+str(topics[-2])+'.csv', index=False)
+    # test_query("NYU Stern",100)
+    # for topic in topics:
+            
+    #     print("The topic scrapped is : "+ topic)
+    #     full_df = get_tweets_from_people(data[topic],number_of_tweets=number_of_tweets)
+    #     full_df.to_csv("df_on_"+str(topic)+'.csv', index=False)
+    #         #Api dépassée
+    # file.close()
+>>>>>>> Stashed changes
